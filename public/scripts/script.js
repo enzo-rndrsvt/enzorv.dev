212044143b65baf66529fcb4aa1a973559746cdd
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-11-17 15:02:06 +0100

    change animation + add skills, contact and footer

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,7 @@
 const sections = [
-  { trigger: "apropos", target: "aproposCible" },
-  { trigger: "projets", target: "projetsCible" },
-  { trigger: "contact", target: "contactCible" },
+  { trigger: "apropos", target: "aproposCible", block: "center" },
+  { trigger: "projets", target: "projetsCible", block: "start" },
+  { trigger: "contact", target: "contactCible", block: "center" },
 ];
 
 // Fonction pour ajouter la classe 'active-link' au lien actif
@@ -15,14 +15,33 @@ function activateLink(linkId) {
 
 
 // Écouteur d'événements pour le clic, pour faire défiler vers la section correspondante
-sections.forEach(({ trigger, target }) => {
+sections.forEach(({ trigger, target, block }) => {
   const element = document.getElementById(trigger);
   const cible = document.getElementById(target);
 
   element.addEventListener("click", function (e) {
     e.preventDefault();
-    cible.scrollIntoView({ behavior: "smooth" });
+    cible.scrollIntoView({ behavior: "smooth", block: block });
   });
 });
+
+
+
+// Fonction pour ouvrir la prévisualisation
+function openPreview(imageElement) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  
+  // Met à jour l'image de la modale avec la source de l'image cliquée
+  lightboxImg.src = imageElement.src;
+  lightbox.style.display = "flex"; // Affiche la modale
+}
+
+// Fonction pour fermer la prévisualisation
+function closePreview() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none"; // Cache la modale
+}
+
 
 
